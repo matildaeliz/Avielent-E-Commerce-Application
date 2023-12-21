d4388b06eb435642dbedd8b14869452408ce9756
BugraBedir <bedirbugra@gmail.com>
2023-12-21 17:31:17 +0300

    UserEntity variables and methods has been added.

--- UserService/src/main/java/com/example/userservice/Entity/UserEntity.java
+++ UserService/src/main/java/com/example/userservice/Entity/UserEntity.java
@@ -1,39 +1,44 @@
 package com.example.userservice.Entity;
-
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "userinfo")
+@Table(name="userinfo")
 public class UserEntity {
     @Id
     @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     @Column
     private String username;
     @Column
-    private String  password;
+    private String password;
+    @Column
+    private double balance;
 
     public UserEntity(String username,String password){
-        this.username = username;
-        this.password = password;
+        this.username=username;
+        this.password=password;
     }
-
-    public UserEntity() {
+    public UserEntity(){
 
     }
-
-
-    public String getUsername() {
+    public String getUsername(){
         return username;
     }
-    public String getPassword() {
+    public String getPassword(){
         return password;
     }
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPassword(String password){
+        this.password=password;
     }
-    public int getId() {
+    public int getId(){
         return id;
     }
+    public int getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance=balance;
+    }
+
 }
