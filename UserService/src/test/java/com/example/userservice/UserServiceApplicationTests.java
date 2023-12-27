76e4ecd1700ea69a0473a9df060a6e4247edb538
Arda Onur <104937816+matildaeliz@users.noreply.github.com>
2023-12-27 15:49:50 +0300

    Implemented a method that when the push add card button a related product information sent the related page and Test test method implemented

--- UserService/src/test/java/com/example/userservice/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/example/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.userservice;
 
+import com.example.userservice.Response.UserResponse;
+import com.example.userservice.Service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
+    @Autowired
+ UserService userService;
     @Test
     void contextLoads() {
     }
+@Test
 
+
+
+   void createUsername(){
+      ResponseEntity a =  userService.addUser(new UserResponse("ardafhgh5tghg","3232","ardaonur15",0));
+
+          assertEquals(a.getStatusCode(),HttpStatus.OK);
+   }
 }
