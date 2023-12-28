f3a160b9544fd3790ba4b8c90fcf70c985030908
Arda Onur <104937816+matildaeliz@users.noreply.github.com>
2023-12-28 20:17:25 +0300

    Implemented tests

--- OrderService/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
+++ OrderService/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.orderservice;
 
+import com.example.orderservice.Service.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
-
+    @Autowired
+    OrderService orderService;
     @Test
     void contextLoads() {
     }
+    @Test
+    void CheckBalanceWhileOrderTest(){
+        ResponseEntity a = orderService.addOrder("TXT2000","Graphic Card","3000","Ardam","5");
+
+        assertEquals(HttpStatus.BAD_REQUEST,a.getStatusCode());
+    }
+
+    @Test
+    void makeOrderTest(){
+        ResponseEntity a = orderService.addOrder("TXT2000","Graphic Card","3000","Ardam","32325");
+        assertEquals(HttpStatus.OK,a.getStatusCode());
+    }
+
 
 }
--- ProductService/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ ProductService/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.productservice;
 
+import com.example.productservice.Service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class ProductServiceApplicationTests {
+    @Autowired
+    ProductService productService;
 
     @Test
     void contextLoads() {
     }
 
+
+
+    @Test
+    void addProduct(){
+        ResponseEntity a =  productService.addProduct("TXT2000","Graphic Card","3000","Diyar","Resim");
+        assertEquals(HttpStatus.OK,a.getStatusCode());
+    }
+
+    @Test
+    void AddProductFailTest(){
+        ResponseEntity a = productService.addProduct("3000","","","322","23");
+
+        assertEquals(HttpStatus.BAD_REQUEST,a.getStatusCode());
+    }
+    @Test
+    void ListProduct() throws JsonProcessingException {
+       ResponseEntity a =  productService.listRelatedProduct("rtx");
+        assertEquals(HttpStatus.OK,a.getStatusCode());
+    }
 }
